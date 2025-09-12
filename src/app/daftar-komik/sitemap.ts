825557475af718b93d857d8076f997551ccba80a
alfin <muhamadalfinpratamaa@gmail.com>
2025-09-12 20:30:39 +0700

    feat: Update dynamic rendering setting in sitemap to force-dynamic

--- src/app/daftar-komik/sitemap.ts
+++ src/app/daftar-komik/sitemap.ts
@@ -4,7 +4,7 @@ import { getAllComicsForSitemap } from "@/action/comics";
 import { siteUrl } from "@/app/sitemap";
 import { MetadataRoute } from "next";
 
-export const dynamic = "edge";
+export const dynamic = "force-dynamic";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   try {
