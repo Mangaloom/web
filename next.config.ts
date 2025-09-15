6e79782544f5f1fbaf1fe927ca5dd74173ee191b
alfin <muhamadalfinpratamaa@gmail.com>
2025-09-15 23:14:42 +0700

    fix: Update remotePatterns in Next.js config to allow subdomains for komikcast.li

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // all domain allowed
     remotePatterns: [
       {
         protocol: "https",
         hostname: "komikcast.li",
+      },
+      {
+        protocol: "https",
+        hostname: "*.komikcast.li",
       },
     ],
   },
